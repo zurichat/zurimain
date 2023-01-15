dc96105a426cb48d9b26ccf43b78505126d1c263
Obi Fortune <gabrielobi.of@gmail.com>
2023-01-15 15:19:13 +0100

    removed sidebar to be implemented in the ui package

--- packages/sidebar/src/Sidebar.tsx
+++
@@ -1,5 +0,0 @@
-export const Sidebar = () => {
-  return <>sidebar</>;
-};
-
-export default Sidebar;
--- packages/sidebar/src/root.component.tsx
+++
@@ -1,5 +0,0 @@
-import Sidebar from "./Sidebar";
-
-export default function Root() {
-  return <Sidebar />;
-}
