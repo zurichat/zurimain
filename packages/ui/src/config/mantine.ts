827908d674595ab34164d003fcfd68d8ac4aa753
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-23 09:07:19 +0100

    chore: primary theme setup

--- packages/ui/src/config/mantine.ts
+++ packages/ui/src/config/mantine.ts
@@ -1,7 +1,32 @@
-import { MantineTheme } from "@mantine/core";
+import { MantineThemeOverride } from "@mantine/core";
 
-export const mantineConfig: Partial<MantineTheme> = {
+import { Tuple, DefaultMantineColor } from "@mantine/core";
+
+type ExtendedCustomColors = "primary" | DefaultMantineColor;
+
+declare module "@mantine/core" {
+  export interface MantineThemeColorsOverride {
+    colors: Record<ExtendedCustomColors, Tuple<string, 10>>;
+  }
+}
+
+export const mantineConfig: Partial<MantineThemeOverride> = {
   fontFamily: "Lato",
-  colorScheme: "light"
+  colorScheme: "light",
+  colors: {
+    primary: [
+      "#CEFFEF",
+      "#71B7A0",
+      "#55B898",
+      "#3CB990",
+      "#28B788",
+      "#14B681",
+      "#00B87C",
+      "#11956A",
+      "#1B7A5B",
+      "#21654F"
+    ]
+  },
+  primaryColor: "primary"
 };
 export default mantineConfig;
