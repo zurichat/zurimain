a52f2ac7121ca05364e0935e18fefd5d56d3bcd1
Obi Fortune <gabrielobi.of@gmail.com>
2023-02-27 15:34:06 +0100

    updated the page layout

--- packages/ui/src/onboarding/Login/LoginWrapper/index.tsx
+++ packages/ui/src/onboarding/Login/LoginWrapper/index.tsx
@@ -1,12 +1,12 @@
-import { Paper, createStyles, Flex } from "@mantine/core";
-import FormHeader from "../components/FormHeader";
+import { createStyles, Flex } from "@mantine/core";
 import { Outlet } from "react-router-dom";
 import { withMantine } from "../../../Wrappers/Mantine";
+import FormHeader from "../components/FormHeader";
 
 const useStyles = createStyles(theme => ({
   pageContainer: {
     height: "100vh",
-    background: "#FCF7F1"
+    background: theme.colors["secondary"][4]
   },
 
   wrapper: {
@@ -16,24 +16,10 @@ const useStyles = createStyles(theme => ({
   },
 
   formContainer: {
-    width: "60%",
+    width: "100%",
     height: "100%",
     positon: "relative",
-    padding: "17px",
-    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-      width: "100%"
-    }
-  },
-
-  formImage: {
-    width: "40%",
-    height: "100%",
-    background: "#14466F",
-    color: "white",
-    zIndex: 99,
-    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-      display: "none"
-    }
+    padding: "17px"
   }
 }));
 
@@ -41,12 +27,9 @@ export const LoginWrapper: React.FC = withMantine(() => {
   const { classes } = useStyles();
   return (
     <div className={classes.pageContainer}>
-      <Flex className={classes.wrapper} justify="between" align="center">
-        <Flex className={classes.formContainer} justify="center" align="center">
-          <FormHeader />
-          <Outlet />
-        </Flex>
-        <Paper className={classes.formImage} />
+      <Flex className={classes.formContainer} justify="center" align="center">
+        <FormHeader />
+        <Outlet />
       </Flex>
     </div>
   );
